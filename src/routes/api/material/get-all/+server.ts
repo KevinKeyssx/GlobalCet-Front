b3006ff5003811fe1950b517d78139d6c83c6ba9
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 00:12:30 -0400

    Material get all server added

---
+++ src/routes/api/material/get-all/+server.ts
@@ -0,0 +1,18 @@
+import { json }                  from '@sveltejs/kit';
+import type { RequestHandler }   from './$types';
+import connectRequest            from '$lib/services/fetch.service';
+import { EXTERNAL_ENDPOINTS }    from '$lib/utils/endpoints';
+import { ENV }                   from '$lib/utils/env.server';
+
+export const GET: RequestHandler = async ( { fetch } ) => {
+	const response = await connectRequest( {
+		endpoint   : EXTERNAL_ENDPOINTS.MATERIALS.GET_ALL,
+		isInternal : false,
+		headers    : {
+			'x-secret' : ENV.INTERNAL_SECRET_KEY,
+		},
+		fetch,
+	} );
+
+	return json( response );
+};

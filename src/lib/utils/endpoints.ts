3bf2bed74ad7f442b09de87209dfc2b3c02cc666
kevinkeyssx <keyss1700@gmail.com>
2026-05-24 00:08:42 -0400

    INTERNAL_ENDPOINTS & EXTERNAL_ENDPOINTS added

---
+++ src/lib/utils/endpoints.ts
@@ -0,0 +1,29 @@
+export const INTERNAL_ENDPOINTS = {
+	PRODUCTS      : {
+		TECHNICAL_SPECS : 'products/get-technical-specs',
+	},
+	GLOBAL_SEARCH : {
+		BASE : 'global-search',
+	},
+	CATEGORIES    : {
+		GET_ALL : 'categories/get-all',
+	},
+	MATERIALS     : {
+		GET_ALL : 'material/get-all',
+	},
+};
+
+export const EXTERNAL_ENDPOINTS = {
+	PRODUCTS      : {
+		BASE : '/products',
+	},
+	GLOBAL_SEARCH : {
+		BASE : '/search',
+	},
+	CATEGORIES    : {
+		GET_ALL : '/categories',
+	},
+	MATERIALS     : {
+		GET_ALL : '/materials',
+	},
+};

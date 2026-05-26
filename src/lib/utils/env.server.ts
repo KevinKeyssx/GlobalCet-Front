2bc3ab3e56f0b5808c58acc51f9d2fb08a75b18a
kevinkeyssx <keyss1700@gmail.com>
2026-05-25 23:15:10 -0400

    FILE_MANAGER envs implemented

--- src/lib/utils/env.server.ts
+++ src/lib/utils/env.server.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 import { 
     INTERNAL_SECRET_KEY,
     BACKEND_URL,
-    FRONTEND_URL
+    FRONTEND_URL,
+    FILE_MANAGER_URL,
+    FILE_MANAGER_FOLDER
 } from '$env/static/private';
 
 
@@ -11,13 +13,18 @@ const envSchema = z.object({
     BACKEND_URL         : z.string().min(1),
     FRONTEND_URL        : z.url().min(1),
     INTERNAL_SECRET_KEY : z.string().min(1),
+
+    FILE_MANAGER_URL        : z.string().min(1),
+    FILE_MANAGER_FOLDER : z.string().min(1),
 });
 
 
 const parsedEnv = envSchema.safeParse({
     INTERNAL_SECRET_KEY,
     BACKEND_URL,
-    FRONTEND_URL
+    FRONTEND_URL,
+    FILE_MANAGER_URL,
+    FILE_MANAGER_FOLDER
 });
 
 
@@ -35,4 +42,9 @@ export const ENV = {
     FRONTEND_URL    : parsedEnv.data.FRONTEND_URL,
 
     INTERNAL_SECRET_KEY: parsedEnv.data.INTERNAL_SECRET_KEY,
+
+    FILE_MANAGER: {
+        URL     : parsedEnv.data.FILE_MANAGER_URL,
+        FOLDER  : parsedEnv.data.FILE_MANAGER_FOLDER
+    }
 };

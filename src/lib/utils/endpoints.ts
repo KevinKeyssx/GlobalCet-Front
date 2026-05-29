bbac97e8b132b2c09eb6e114dcb4e4bfc7779166
kevinkeyssx <keyss1700@gmail.com>
2026-05-28 23:07:42 -0400

    INTERNAL_ENDPOINTS & EXTERNAL_ENDPOINTS updated

--- src/lib/utils/endpoints.ts
+++ src/lib/utils/endpoints.ts
@@ -1,41 +1,52 @@
 export const INTERNAL_ENDPOINTS = {
 	PRODUCTS       : {
+        FILTERS         : 'products/filters',
 		TECHNICAL_SPECS : 'products/get-technical-specs',
+        MATERIALS       : {
+            GET_ALL : 'products/materials/get-all',
+        },
+        CATEGORIES     : {
+            GET_ALL : 'products/categories/get-all',
+        },
 	},
+    KITS : {
+        FILTERS : 'kits/filters',
+        CATEGORIES : {
+            GET_ALL : 'kits/categories/get-all',
+        },
+    },
+    LABS : {
+        FILTERS : 'labs/filters',
+        CATEGORIES : {
+            GET_ALL : 'labs/categories/get-all',
+        },
+    },
 	GLOBAL_SEARCH  : {
 		BASE : 'global-search',
-	},
-	CATEGORIES     : {
-		GET_ALL : 'categories/get-all',
-	},
-	MATERIALS      : {
-		GET_ALL : 'material/get-all',
-	},
-	KIT_CATEGORIES : {
-		GET_ALL : 'kit-categories/get-all',
-	},
-	LAB_CATEGORIES : {
-		GET_ALL : 'lab-categories/get-all',
 	},
 };
 
 export const EXTERNAL_ENDPOINTS = {
 	PRODUCTS       : {
 		BASE : '/products',
+        MATERIALS      : {
+            GET_ALL : '/materials',
+        },
+        CATEGORIES     : {
+            GET_ALL : '/categories',
+        },
 	},
 	GLOBAL_SEARCH  : {
 		BASE : '/global-searches',
 	},
-	CATEGORIES     : {
-		GET_ALL : '/categories',
-	},
-	MATERIALS      : {
-		GET_ALL : '/materials',
-	},
-	KIT_CATEGORIES : {
-		GET_ALL : '/kit-categories',
-	},
-	LAB_CATEGORIES : {
-		GET_ALL : '/lab-categories',
-	},
+    KITS: {
+        CATEGORIES : {
+            GET_ALL : '/kit-categories',
+        },
+    },
+    LABS: {
+        CATEGORIES : {
+            GET_ALL : '/lab-categories',
+        },
+    }
 };

0a5f9bca8fae7d2a20ea30e6115dcaccd2311e92
kevinkeyssx <keyss1700@gmail.com>
2026-06-08 22:39:29 -0400

    browser implemented

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,18 +1,22 @@
 import { QueryClient } from '@tanstack/svelte-query';
-import { browser } from '$app/environment';
+import { browser }     from '$app/environment';
 import type { LayoutLoad } from './$types';
 
+let queryClient: QueryClient | undefined;
+
 export const load: LayoutLoad = async () => {
-	const queryClient = new QueryClient( {
-		defaultOptions : {
-			queries : {
-				staleTime            : 1000 * 60 * 5,
-				retry                : 3,
-				refetchOnWindowFocus : false,
-				enabled              : browser,
+	if ( !queryClient || !browser ) {
+		queryClient = new QueryClient( {
+			defaultOptions : {
+				queries : {
+					staleTime				: 1000 * 60 * 5,
+					retry					: 3,
+					refetchOnWindowFocus	: false,
+					enabled					: browser,
+				},
 			},
-		},
-	} );
+		} );
+	}
 
 	return { queryClient };
 };
